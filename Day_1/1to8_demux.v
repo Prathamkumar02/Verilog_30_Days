d1138192eed628b8bdb4da39df26bddd286b9cf9
Prathamkumar02 <111525146+Prathamkumar02@users.noreply.github.com>
2025-10-28 18:23:02 +0530

    Update 1to8_demux.v

--- Day_1/1to8_demux.v
+++ Day_1/1to8_demux.v
@@ -1,3 +1,12 @@
+/*Design a 3:8 Demultiplexer in Verilog using the
+behavioral modeling style. The module should
+have a 1-bit input (data_in), a 3-bit select line (sel),
+and 8 output lines (data_out[7:0]). Use an always
+block to implement the logic, ensuring that only
+one output is set to the value of data_in based on
+the select lines, while all other outputs are set to
+zero.*/
+
 module Demux(output reg[7:0] data_out,
             input [2:0] sel,
             input data_in);
