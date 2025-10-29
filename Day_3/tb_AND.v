9ce4906714753e7c34b543987e0a0cc770768d87
Prathamkumar02 <111525146+Prathamkumar02@users.noreply.github.com>
2025-10-29 22:40:01 +0530

    Update tb_AND.v

--- Day_3/tb_AND.v
+++ Day_3/tb_AND.v
@@ -1,1 +1,18 @@
 
+
+module tb_AND();
+    reg req,ack;
+    wire y;
+   //Instantiation
+   AND uut(.req(req),.ack(ack),.y(y));
+ initial
+ begin
+ $monitor("%t : req=%b ,ack = %b , y = %b",$time,req,ack,y);
+    req=0 ; ack=0;#10;
+    req=0 ; ack=1;#10;
+    req=1 ; ack=0;#10;
+    req=1 ; ack=1;#10;
+    #100 $finish;
+end
+endmodule
+
